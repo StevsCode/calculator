b6497727351b1345ac8aaf8c57172a6f6710275a
StevsCode <stevensavarese1@gmail.com>
2025-02-26 15:10:55 +0100

    Delete and replace unnecessary code

--- script.js
+++ script.js
@@ -4,46 +4,23 @@ let lastNum = 0;
 
 // Add
 function addResult(a, b) {
-    const addArray = new Array(a, b);
-    return addArray
-        .reduce((acc, curr) => acc + curr, 0);
+    return a + b;
 };
-console.log(addResult(firstNum, lastNum));
 
 // Subtract
 function subtractResult(a, b) {
-    const subtractArray = new Array(a, b);
-    return subtractArray
-        .reduce((acc, curr) => acc - curr, 0);
+    return a - b;
 };
-console.log(subtractResult(firstNum, lastNum));
 
 // Multiply
 function multiplyResult(a, b) {
-    const multiplyArray = new Array(a, b);
-    return multiplyArray
-        .reduce((acc, curr) => acc * curr, 1);
+    return a * b;
 };
-console.log(multiplyResult(firstNum, lastNum));
 
 // Divide
 function divideResult(a, b) {
-    const divideArray = new Array(a, b);
-    return divideArray
-        .reduce((acc, curr) => acc / curr, 0);
+    return a / b;
 };
-console.log(divideResult(firstNum, lastNum));
-
-
-// Create variables for each part of the operation
-// First number
-// const firstNum = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
-
-// Operator
-// const operator = new Array("+", "-", "*", "/");
-
-// Second number
-// const secondNum = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
 
 
 // Create function that makes the operation
