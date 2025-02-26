e0f23be1c06ea2e27f970fc241dc7173e4c90cf2
StevsCode <stevensavarese1@gmail.com>
2025-02-26 15:06:09 +0100

    Operate on the numbers when = is pressed

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // Create functions for the four operations and test them
+let firstNum = 0;
+let lastNum = 0;
+
 // Add
 function addResult(a, b) {
     const addArray = new Array(a, b);
     return addArray
         .reduce((acc, curr) => acc + curr, 0);
 };
-console.log(addResult(10, 5));
+console.log(addResult(firstNum, lastNum));
 
 // Subtract
 function subtractResult(a, b) {
@@ -13,7 +16,7 @@ function subtractResult(a, b) {
     return subtractArray
         .reduce((acc, curr) => acc - curr, 0);
 };
-console.log(subtractResult(10, 5));
+console.log(subtractResult(firstNum, lastNum));
 
 // Multiply
 function multiplyResult(a, b) {
@@ -21,7 +24,7 @@ function multiplyResult(a, b) {
     return multiplyArray
         .reduce((acc, curr) => acc * curr, 1);
 };
-console.log(multiplyResult(10, 5));
+console.log(multiplyResult(firstNum, lastNum));
 
 // Divide
 function divideResult(a, b) {
@@ -29,30 +32,30 @@ function divideResult(a, b) {
     return divideArray
         .reduce((acc, curr) => acc / curr, 0);
 };
-console.log(divideResult(10, 5));
+console.log(divideResult(firstNum, lastNum));
 
 
 // Create variables for each part of the operation
 // First number
-const firstNum = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+// const firstNum = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
 
 // Operator
-const operator = new Array("+", "-", "*", "/");
+// const operator = new Array("+", "-", "*", "/");
 
 // Second number
-const secondNum = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+// const secondNum = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
 
 
 // Create function that makes the operation
 function operate(operator, firstNum, secondNum) {
     if (operator === "+") {
-        return addResult(firstNum, secondNum)
+        return addResult(firstNum, secondNum);
     } else if (operator === "-") {
-        return subtractResult(firstNum, secondNum)
+        return subtractResult(firstNum, secondNum);
     } else if (operator === "*") {
-        return multiplyResult(firstNum, secondNum)
+        return multiplyResult(firstNum, secondNum);
     } else {
-        return divideResult(firstNum, secondNum)
+        return divideResult(firstNum, secondNum);
     };
 };
 
@@ -75,30 +78,26 @@ function displayFun() {
 
 
 // Store the first and second numbers
-let operatorVar = document.querySelectorAll(".operator");
-operatorVar.forEach(button => {
+let operatorBox = document.querySelectorAll(".operator");
+operatorBox.forEach(button => {
     button.addEventListener("click", saveFirst);
 });
-let elementOne = 0;
 let operated = false;
+let operatorVar = "+";
 function saveFirst() {
-    elementOne = displayVar.textContent;
+    firstNum = parseFloat(displayVar.textContent);
+    operatorVar = this.textContent
     displayVar.textContent = this.textContent;
     operated = true;
 };
+
 // Operate on the numbers when = is pressed
 let equalsVar = document.querySelector(".equals");
 equalsVar.addEventListener("click", saveLast);
-let elementTwo = 0;
 function saveLast() {
-    elementTwo = displayVar.textContent;
-    if (operatorVar.textContent === "+") {
-        addResult(elementOne, elementTwo);
-    } else if (operatorVar.textContent === "-") {
-        subtractResult(elementOne, elementTwo);
-    } else if (operatorVar.textContent === "*") {
-        multiplyResult(elementOne, elementTwo);
-    } else if (operatorVar.textContent === "/") {
-        divideResult(elementOne, elementTwo);
-    };
+    lastNum = parseFloat(displayVar.textContent);
+    operate(operatorVar, firstNum, lastNum);
+    let result = operate(operatorVar, firstNum, lastNum);
+    displayVar.textContent = result;
+    operated = true;
 };
